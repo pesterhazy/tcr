694ce07e4eb7665635ed7e91b465587d19d2a4b1
Paulus Esterhazy <pesterhazy@gmail.com>
2022-06-25 20:08:23 +0200

    WIP

--- watch.clj
+++ watch.clj
@@ -1,19 +1,26 @@
-(require '[babashka.process :refer [process check]])
+(require '[babashka.process :refer [process shell]])
 
 (def !watch-process (atom nil))
 
 (defn wait []
   (when-not @!watch-process
-    (reset! !watch-process (process ["watchexec" "-p" "echo" "."])))
+    (reset! !watch-process (process ["watchexec" "-p" "-w" "tcr.mjs" "echo" "."])))
 
   (binding [*in* (clojure.java.io/reader (:out @!watch-process))]
     (read-line)))
 
 (defn act []
-  (prn :act)
-  )
+  (println "Running...")
+  (try
+    (shell "node tcr.mjs")
+    (catch clojure.lang.ExceptionInfo e
+      (when-not (:exit (ex-data e))
+        (throw e))
+      false)
+    true))
 
 (loop []
-  (act)
+  (when-not (act)
+    (prn :boom))
   (wait)
   (recur))
