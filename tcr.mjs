2dc5650b1d78723ce017c9fe6b40878ad30bac69
Paulus Esterhazy <pesterhazy@gmail.com>
2022-06-26 22:06:26 +0200

    autocommit

--- tcr.mjs
+++ tcr.mjs
@@ -4,6 +4,15 @@ function rpn(s) {
   let tokens = s.split(/ /);
   let args = [];
 
+  for (let token of tokens) {
+    switch (token) {
+    case "+":
+      break;
+
+    default:
+      args.push(parseInt(token));
+    }
+  }
   args[0] = parseInt(tokens[0]);
   args[1] = parseInt(tokens[1]);
   return args[0] + args[1];
