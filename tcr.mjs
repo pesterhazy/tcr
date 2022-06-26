e449d413be2236c87b16a960728663aba6215f6c
Paulus Esterhazy <pesterhazy@gmail.com>
2022-06-26 22:03:23 +0200

    autocommit

--- tcr.mjs
+++ tcr.mjs
@@ -2,6 +2,10 @@ import * as assert from "node:assert/strict";
 
 function rpn(s) {
   let tokens = s.split(/ /);
+  let args = [];
+
+  args[0] = parseInt(tokens[0]);
+  args[1] = parseInt(tokens[1]);
   return 3;
 }
 
