413a50b55883eb917555ca9773db4f7389984304
Paulus Esterhazy <pesterhazy@gmail.com>
2022-06-26 18:43:56 +0200

    autocommit

--- tcr.mjs
+++ tcr.mjs
@@ -7,15 +7,22 @@ function splitWithEOL(s) {
 function find(s) {
   let r = /((?!\n).*)\n/y;
   let result = [];
+  let last=0;
+
   do {
     let m = r.exec(s);
     if ( !m )
       break;
 
+    last = r.lastIndex;
     let match = m[0];
-
-    return [match,s.slice(r.lastIndex)];
+    result.push(match);
   } while (1);
+
+  if (last < s.length)
+    result.push(s.slice(last));
+
+  return result;
 }
 
 assert.deepEqual(["a\n","b"],find("a\nb"));
