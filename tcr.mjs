4a3e161e85a183cb7253d759581f7413f2ba2d3a
Paulus Esterhazy <pesterhazy@gmail.com>
2022-06-26 18:04:39 +0200

    wip

--- tcr.mjs
+++ tcr.mjs
@@ -1,9 +1,17 @@
 import * as assert from "node:assert/strict";
 
 function splitWithEOL(s) {
-  let r = /((?!\n).*)\n/;
   return s.split(/\n/);
 }
+
+function find(s) {
+  let r = /((?!\n).*)\n/;
+  let m = r.exec(s);
+  return m[0];
+}
+
+assert.deepEqual("a\n",find("a\nb"));
+
 
 assert.deepEqual([""], splitWithEOL(""));
 assert.deepEqual(["a"], splitWithEOL("a"));
